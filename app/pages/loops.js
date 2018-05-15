960d15f17119b34d96b73e9eab54076f1b747dc2
Akbalstalker <Rockfrees@mail.ru>
2018-05-16 01:52:03 +0300

    Перекидываю 2 домашку на ноут

---
+++ app/pages/loops.js
@@ -0,0 +1,11 @@
+// const isAdmin = true;
+
+// if (isAdmin) {
+// 	alert (`Привет Admin!`);
+// } else {
+// 	alert (`Привет, ты не Админ`);
+// }
+
+const isAdmin = true;
+const result = (isAdmin)? `Привет Admin!` : `Привет, ты не Админ`
+alert(result)

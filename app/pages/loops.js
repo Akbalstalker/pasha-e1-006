6188fbf50a3cc8e10b04d5d57cced65ee2332d33
Akbalstalker <Rockfrees@mail.ru>
2018-05-19 19:06:25 +0300

    осталось совсем чуть-чуть

--- app/pages/loops.js
+++ app/pages/loops.js
@@ -7,5 +7,25 @@
 // }
 
 const isAdmin = true;
-const result = (isAdmin)? `Привет Admin!` : `Привет, ты не Админ`
-alert(result)
+const result = (isAdmin)? `Привет Admin!` : `Привет, ты не Админ`;
+alert(result);
+
+// Перепиши if...else на switch:
+
+const age = +prompt('Сколько тебе лет?', '12');
+
+// if (age === 12) {
+// 	alert('Слишком мало :(');
+// } else if (age === 16) {
+// 	alert('Почти достаточно :[');
+// } else if (age === 18) {
+// 	alert('Вот сейчас в самый раз! :)');
+// } else {
+// 	alert('Возраст не определен %)');
+// }
+
+switch (age){
+	case 12 : alert ('Слишком мало :('); break;
+	case 16 : alert ('Почти достаточно :['); break;
+	case 18 : alert ('Вот сейчас в самый раз! :)'); break;
+	default : alert('Возраст не определен %)');
